30234b2634b72e4e3dc927e14de31832dad7af6e
Heem <42186537+HeemPlayz@users.noreply.github.com>
2019-06-01 11:44:42 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const request = require('request');
 
 const config = require('./config.json');
 
-let ownerID = "402043862480322562";
+let ownerID = "523836549390139392";
 
 let voiceChannel;
 let conn;
@@ -34,7 +34,7 @@ client.on('message', (message) => {
     switch (command) {
 
         case "help":
-            if (message.channel.id !== config.channels.text) return;
+           // if (message.channel.id !== config.channels.text) return;
             if (args.length > 0) return;
             message.channel.send(`**__Music Commands:__**\n\n**All users**\n\n\`${config.bot.prefix}join\`\n\`\`\`Summon the bot to your current voice channel.\`\`\`\n\n\`${config.bot.prefix}leave\`\n\`\`\`Remove the bot from the voice channel.\`\`\`\n\n\`${config.bot.prefix}play\`\n\`\`\`Request a song to be added to the playlist queue. The song specified can be a YouTube video URL or a song name.\`\`\`\n\n\`${config.bot.prefix}remove\` \`[song number]\`\n\`\`\`Remove a song that you have previously requested from the playlist queue, at the specified position. Note: Users with the mod/DJ role can remove any song, regardless of who requested it.\`\`\`\n\n\`${config.bot.prefix}song\`\n\`\`\`Display the song that is currently playing.\`\`\`\n\n\`${config.bot.prefix}queue\`\n\`\`\`Display the songs currently in the queue.\`\`\`\n\n**Try Administrator/الأدارة only**\n\n\`${config.bot.prefix}resume\`\n\`\`\`Resume music playback if it is currently paused.\`\`\`\n\n\`${config.bot.prefix}pause\`\n\`\`\`Pause music playback if it is currently playing.\`\`\`\n\n\`${config.bot.prefix}skip\`\n\`\`\`Skip the current song and start playing the next song in the queue.\`\`\`\n\n\`${config.bot.prefix}clear\`\n\`\`\`Clear the playlist queue.\`\`\``);
             break;
@@ -55,7 +55,7 @@ client.on('message', (message) => {
             break;
 
         case "join":
-            if (message.channel.id !== config.channels.text) return;
+           // if (message.channel.id !== config.channels.text) return;
             if (args.length > 0) return;
             voiceChannel = message.member.voiceChannel;
             if (!voiceChannel) return message.channel.send(":x: You are not in a voice channel.");
@@ -68,7 +68,7 @@ client.on('message', (message) => {
             break;
 
         case "leave":
-            if (message.channel.id !== config.channels.text) return;
+           // if (message.channel.id !== config.channels.text) return;
             if (args.length > 0) return;
             inVoiceChannel = client.voiceConnections.size > 0;
             if (!inVoiceChannel) return message.channel.send(":x: I am not in a voice channel.");
@@ -85,11 +85,11 @@ client.on('message', (message) => {
             break;
 
         case "play":
-            if (message.channel.id !== config.channels.text) return;
+           
             if (args.length === 0) return;
             voiceChannel = message.member.voiceChannel;
             if (!voiceChannel) return message.channel.send(":x: You are not in a voice channel.");
-            if (voiceChannel.id !== config.channels.voice) return;
+           
             inVoiceChannel = client.voiceConnections.size > 0;
             let s = args[0];
             message.channel.send(":mag_right: Searching...").then(msg => {
@@ -158,7 +158,7 @@ client.on('message', (message) => {
             break;
 
         case "remove":
-            if (message.channel.id !== config.channels.text) return;
+          
             let removeIndex;
             if (args.length === 0) removeIndex = playlist.length - 1;
             if (args.length === 1) removeIndex = Number(args[0]) - 1;
@@ -176,7 +176,7 @@ client.on('message', (message) => {
             break;
 
         case "clear":
-            if (message.channel.id !== config.channels.text) return;
+           // if (message.channel.id !== config.channels.text) return;
             if (args.length > 0) return;
             if (!hasPerms()) return message.channel.send(":no_entry_sign: You do not have permission to use this command.");
             playlist = [];
@@ -206,7 +206,7 @@ function play(message) {
         let cmd = m.content.slice(config.bot.prefix.length);
         switch (cmd) {
             case "pause":
-                if (m.channel.id !== config.channels.text) return;
+               // if (m.channel.id !== config.channels.text) return;
                 if (!hasPerms()) return m.channel.send(":no_entry_sign: You do not have permission to use this command.");
                 if (playing) {
                     m.channel.send(":pause_button: Paused.").then(() => {
@@ -218,7 +218,7 @@ function play(message) {
                 break;
 
             case "resume":
-                if (m.channel.id !== config.channels.text) return;
+              //  if (m.channel.id !== config.channels.text) return;
                 if (!hasPerms()) return m.channel.send(":no_entry_sign: You do not have permission to use this command.");
                 if (!playing) {
                     m.channel.send(":arrow_forward: Resumed.").then(() => {
@@ -230,7 +230,7 @@ function play(message) {
                 break;
 
             case "skip":
-                if (m.channel.id !== config.channels.text) return;
+               // if (m.channel.id !== config.channels.text) return;
                 if (!hasPerms()) return m.channel.send(":no_entry_sign: You do not have permission to use this command.");
                 m.channel.send(":track_next: Skipped.").then(() => {
                     dispatcher.end();
@@ -240,13 +240,13 @@ function play(message) {
                 break;
 
             case "song":
-                if (m.channel.id !== config.channels.text) return;
+              //  if (m.channel.id !== config.channels.text) return;
                 m.channel.send(`**__Now Playing:__**\n\n**${playlist[0].name}**\n\n(requested by **${playlist[0].requester.username}**)`);
                 break;
         }
 
         function hasPerms() {
-            if (m.member.roles.has(config.roles.mod) || m.member.roles.has(config.roles.dj) || m.author.id === ownerID) return true;
+           // if (m.member.roles.has(config.roles.mod) || m.member.roles.has(config.roles.dj) || m.author.id === ownerID) return true;
             return false;
         }
 
@@ -270,52 +270,5 @@ function play(message) {
         });
     });
 }
-const adminprefix = "+";
-const devs = ['402043862480322562', '443696811421466624'];
- 
 
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'ply')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else 
-  if (message.content === (adminprefix + "Percie")) {
-  message.guild.leave();        
-} else  
-if (message.content.startsWith(adminprefix + 'wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else 
-if (message.content.startsWith(adminprefix + 'ls')) {
-client.user.setActivity(argresult , {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-} else     
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : Done :>`)
-return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-}
-  if(message.content === adminprefix + "restart") {
-    if (!devs.includes(message.author.id)) return;
-        message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
-      console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-      console.log(`⚠️ Bot restarting... ⚠️`);
-      console.log("===============================================\n\n");
-      client.destroy();
-      child_process.fork(__dirname + "/bot.js");
-      console.log(`Bot Successfully Restarted`);
-  }
-
-});
 client.login(process.env.BOT_TOKEN);
